dead6a580dca6ee46379b6fd32051b5b32ba81f9
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-26 16:49:13 +0530

    Finished the calculator program

--- src/bin/02_calculator_program.rs
+++ src/bin/02_calculator_program.rs
@@ -1,5 +1,30 @@
 use std::io::{self, Write};
 
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+fn sub(a: i32, b: i32) -> i32 {
+    return a - b;
+}
+fn mul(a: i32, b: i32) -> i32 {
+    return a * b;
+}
+fn div(a: i32, b: i32) -> i32 {
+    return a / b;
+}
+
+fn fadd(a: f32, b: f32) -> f32 {
+    return a + b;
+}
+fn fmul(a: f32, b: f32) -> f32 {
+    return a * b;
+}
+fn fdiv(a: f32, b: f32) -> f32 {
+    return a / b;
+}
+fn fsub(a: f32, b: f32) -> f32 {
+    return a - b;
+}
 fn main() {
     //getting operator from the user
     let mut c: String = String::new();
@@ -24,5 +49,45 @@ fn main() {
 
     let is_int = |s: &str| !s.contains('.');
 
-    if is_int(a) && is_int(b) {}
+    if is_int(a) && is_int(b) {
+        let i_a: i32 = a.parse().unwrap();
+        let i_b: i32 = b.parse().unwrap();
+        let result = match op {
+            '+' => add(i_a, i_b),
+            '-' => sub(i_a, i_b),
+            '*' => mul(i_a, i_b),
+            '/' => {
+                if i_b == 0 {
+                    println!("<Error!> division by zero");
+                    return;
+                }
+                div(i_a, i_b)
+            }
+            _ => {
+                println!("<Error!> unknown operator!");
+                return;
+            }
+        };
+        println!("Result : {} ", result)
+    } else {
+        let f_a: f32 = a.parse().unwrap();
+        let f_b: f32 = b.parse().unwrap();
+        let result = match op {
+            '+' => fadd(f_a, f_b),
+            '-' => fsub(f_a, f_b),
+            '*' => fmul(f_a, f_b),
+            '/' => {
+                if f_b == 0.0 {
+                    println!("<Error!> division by zero");
+                    return;
+                }
+                fdiv(f_a, f_b)
+            }
+            _ => {
+                println!("<Error!> unknown operator!");
+                return;
+            }
+        };
+        println!("Result : {}", result);
+    }
 }

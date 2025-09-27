a6ec42e46072ca2b4848dbbec3436b0a8e4076c4
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-27 17:11:10 +0530

    changed the program slightly

--- src/bin/03_odd_or_even.rs
+++ src/bin/03_odd_or_even.rs
@@ -1,36 +1,33 @@
-use std::io::{self,Write};
+use std::io::{self, Write};
 
-
-fn main()
-{
+fn main() {
     odd_or_even(5);
     odd_or_even_user_in();
 }
 
-fn odd_or_even(v:i64)
-{
-    if v % 2 == 0 
-    {
-        println!("<EVEN> {} is an even number!",v);
+fn odd_or_even(v: i64) {
+    if v % 2 == 0 {
+        println!("<EVEN> {} is an even number!", v);
     } else {
-        println!("<ODD> {} is an odd number!",v);
+        println!("<ODD> {} is an odd number!", v);
     }
 }
-fn odd_or_even_user_in(){
-    let mut v : String = String::new();
+fn odd_or_even_user_in() {
+    let mut v: String = String::new();
     print!("Enter the number you want find whether it is <odd> or <even> :");
-    io::stdout().flush().expect("Failed to flush the output buffer");
+    io::stdout()
+        .flush()
+        .expect("Failed to flush the output buffer");
 
-    io::stdin().read_line(&mut v).expect("Failed the read the input buffer");
+    io::stdin()
+        .read_line(&mut v)
+        .expect("Failed the read the input buffer");
 
-    let num:i64 = match v.trim().parse()
-    {
-        Ok(n) => n,
-        Err(_) =>
-        {
+    match v.trim().parse::<i64>() {
+        Ok(n) => odd_or_even(n),
+        Err(_) => {
             println!("<Error!> invalid input");
-            return ;
+            return;
         }
     };
-    odd_or_even(num);
 }

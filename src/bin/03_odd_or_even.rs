50a754a4fba0589a3f1aa3f7ebf59dd792319d3e
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-27 17:04:38 +0530

    odd_or_EVen.rs simple program

--- src/bin/03_odd_or_even.rs
+++ src/bin/03_odd_or_even.rs
@@ -1,9 +1,13 @@
+use std::io::{self,Write};
+
+
 fn main()
 {
-
+    odd_or_even(5);
+    odd_or_even_user_in();
 }
 
-fn odd_of_even(v:i64)
+fn odd_or_even(v:i64)
 {
     if v % 2 == 0 
     {
@@ -11,4 +15,22 @@ fn odd_of_even(v:i64)
     } else {
         println!("<ODD> {} is an odd number!",v);
     }
+}
+fn odd_or_even_user_in(){
+    let mut v : String = String::new();
+    print!("Enter the number you want find whether it is <odd> or <even> :");
+    io::stdout().flush().expect("Failed to flush the output buffer");
+
+    io::stdin().read_line(&mut v).expect("Failed the read the input buffer");
+
+    let num:i64 = match v.trim().parse()
+    {
+        Ok(n) => n,
+        Err(_) =>
+        {
+            println!("<Error!> invalid input");
+            return ;
+        }
+    };
+    odd_or_even(num);
 }

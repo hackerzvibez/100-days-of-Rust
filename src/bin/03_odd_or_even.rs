09453f9205fab1ea1d964b553331fa0485a7d9a4
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-27 16:49:05 +0530

    odd_or_Even.rs program intial commit

---
+++ src/bin/03_odd_or_even.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+
+}
+
+fn odd_of_even(v:i64)
+{
+    if v % 2 == 0 
+    {
+        println!("<EVEN> {} is an even number!",v);
+    } else {
+        println!("<ODD> {} is an odd number!",v);
+    }
+}
